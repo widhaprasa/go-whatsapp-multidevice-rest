5a1b574b9ef4ab85558db2fd925c7d0d7a644640
widhaprasa <widhaprasa@gmail.com>
2024-10-22 11:43:10 +0700

    Merge with 1.8.0

--- internal/whatsapp/whatsapp.go
+++ internal/whatsapp/whatsapp.go
@@ -274,7 +274,7 @@ func SendText(c echo.Context) error {
 
 	var reqSendMessage typWhatsApp.RequestSendMessage
 	reqSendMessage.RJID = strings.TrimSpace(c.FormValue("msisdn"))
-	reqSendMessage.Message = strings.TrimSpace(c.FormValue("message"))
+	reqSendMessage.Message = strings.Replace(strings.TrimSpace(c.FormValue("message")), "\\n", "\n", -1)
 
 	if len(reqSendMessage.RJID) == 0 {
 		return router.ResponseBadRequest(c, "Missing Form Value MSISDN")
