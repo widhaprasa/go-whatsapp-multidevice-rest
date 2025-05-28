b31edf01f40353c947955943333bf9666c7e1020
widhaprasa <widhaprasa@gmail.com>
2025-05-28 18:50:46 +0700

    Add Context on new whatsmeow function

--- pkg/whatsapp/whatsapp.go
+++ pkg/whatsapp/whatsapp.go
@@ -39,6 +39,7 @@ var WhatsAppClient = make(map[string]*whatsmeow.Client)
 var (
 	WhatsAppClientProxyURL string
 	WhatsAppOS             string
+	Context                = context.Background()
 )
 
 func init() {
@@ -54,7 +55,7 @@ func init() {
 		log.Print(nil).Fatal("Error Parse Environment Variable for WhatsApp Client Datastore URI")
 	}
 
-	datastore, err := sqlstore.New(dbType, dbURI, nil)
+	datastore, err := sqlstore.New(Context, dbType, dbURI, nil)
 	if err != nil {
 		log.Print(nil).Fatal("Error Connect WhatsApp Client Datastore")
 	}
@@ -241,7 +242,7 @@ func WhatsAppLoginPair(jid string) (string, int, error) {
 			}
 
 			// Request Pairing Code
-			code, err := WhatsAppClient[jid].PairPhone(jid, true, whatsmeow.PairClientChrome, WhatsAppOS)
+			code, err := WhatsAppClient[jid].PairPhone(Context, jid, true, whatsmeow.PairClientChrome, WhatsAppOS)
 			if err != nil {
 				return "", 0, err
 			}
@@ -295,13 +296,13 @@ func WhatsAppLogout(jid string) error {
 			WhatsAppPresence(jid, false)
 
 			// Logout WhatsApp Client and Disconnect from WebSocket
-			err = WhatsAppClient[jid].Logout()
+			err = WhatsAppClient[jid].Logout(Context)
 			if err != nil {
 				// Force Disconnect
 				WhatsAppClient[jid].Disconnect()
 
 				// Manually Delete Device from Datastore Store
-				err = WhatsAppClient[jid].Store.Delete()
+				err = WhatsAppClient[jid].Store.Delete(Context)
 				if err != nil {
 					return err
 				}
@@ -983,11 +984,11 @@ func WhatsAppSendLink(ctx context.Context, jid string, rjid string, linkCaption 
 
 		msgContent := &waE2E.Message{
 			ExtendedTextMessage: &waE2E.ExtendedTextMessage{
-				Text:         proto.String(msgText),
-				Title:        proto.String(urlTitle),
-				MatchedText:  proto.String(linkURL),
-				CanonicalURL: proto.String(linkURL),
-				Description:  proto.String(urlDescription),
+				Text:        proto.String(msgText),
+				Title:       proto.String(urlTitle),
+				MatchedText: proto.String(linkURL),
+				// CanonicalURL: proto.String(linkURL),
+				Description: proto.String(urlDescription),
 			},
 		}
 
